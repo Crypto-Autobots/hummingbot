e37c31ce3e9cbee44aa3c9e59eb6865249556337
manhpt <manhhailua@gmail.com>
2022-08-08 00:46:55 +0700

    fix: ignore if 0

--- hummingbot/strategy/hunger/utils.py
+++ hummingbot/strategy/hunger/utils.py
@@ -2,6 +2,8 @@ from math import log10
 
 
 def round_non_zero(num, digits=2):
+    if num == 0:
+        return 0
     if -1 < num < 1:
         dist = round(log10(abs(num))) - 1
         return round(num, max(digits, abs(dist)))
